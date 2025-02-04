99a59f083db6d2e4cb8bd8a774e32ae7199789f7
ali-mahmoud98 <ali98mahmoudr@gmail.com>
2025-02-04 14:22:55 +0200

    Cmake - Testing the main Function

---
+++ 0x12-cmake_test_main/README.md
@@ -0,0 +1,16 @@
+# CMake - Testing the main Function (Google Test/Catch2)
+
+### Commands
+```bash
+cmake . -B build # create a directory called build then use this directory
+cmake --build build # build the project
+
+# Start Testing
+cd build
+ctest
+cd ..
+# End Testing
+
+# run the program
+./build/rooster
+```
---
+++ 0x12-cmake_test_main/src/run_app.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#include "calculator.h"
+
+int run_app (int agc, char** argv) {
+    Calculator calc;
+    std::cout << "add(10,4): " << calc.add(10,4) << std::endl;
+    std::cout << "sub(10,4): " << calc.subtract(10,4) << std::endl;
+    std::cout << "mul(10,4): " << calc.multiply(10,4) << std::endl;
+    std::cout << "div(10,4): " << calc.divide(10,4) << std::endl;
+    return 0;
+}
